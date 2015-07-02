108046c9b2bd76ae291f68f850f7467cbb415b70
Henry Freel <henryfreel@gmail.com>
2015-07-02 15:23:17 -0700

    sets up project files

--- app.js
+++ app.js
@@ -1,11 +1,8 @@
-// 
-// APP.JS
-// PUT YOUR CUSTOM SCRIPTS AND FUNCTIONS HERE
-// 
-
-//WHEN THE DOCUMENT IS FINISHED LOADING RUN THESE SCRIPTS
 $(document).ready(function() {
   console.log($) // JQUERY IS WORKING!
   console.log(_) // UNDERSCORE IS WORKING!
   console.log("Ready to code!");
+
+  
+  
 });
