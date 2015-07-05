d2d3b955c99925443317edd6ecf5f00660202943
Henry Freel <henryfreel@gmail.com>
2015-07-04 17:00:42 -0700

    adds jquery variables

--- app.js
+++ app.js
@@ -1,8 +1,24 @@
-$(document).ready(function() {
-  console.log($) // JQUERY IS WORKING!
-  console.log(_) // UNDERSCORE IS WORKING!
-  console.log("Ready to code!");
+$(function() {
 
-  
-  
+	// Template
+	var postTemplate = _.template($("#post-template").html(
+
+	// Post Variables
+	var $newPost = $("#new-post")
+
+	var $postName = $("#post-name");
+	var $postBody = $("#post-body");
+
+	var $postsContainer = $("#posts-container")
+
+	// Initial Set up
+	$postName.prop('required',true);
+	$postBody.prop('required',true);
+
+	// Contrsuctor
+	var Post = function(name, body) {
+		this.name = name;
+		this.body = body;
+	}
+
 });
