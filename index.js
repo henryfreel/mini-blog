f702b4989eb6f1b22ac8080807aab03f802e1b50
Henry Freel <henryfreel@gmail.com>
2015-07-11 22:42:32 -0700

    edit post modal is populated with data from server

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 // require express framework and additional modules
 var express = require("express"),
     app = express(),
-    bodyParser = require("body-parser");
+    bodyParser = require("body-parser"),
+    _ = require("underscore");
+
 
 // tell app to use bodyParser middleware
 app.use(bodyParser.urlencoded({extended: true}));
@@ -20,16 +22,18 @@ app.all('/*', function(req, res, next) {
 
 var posts = [
   {
-    id: 1,
+    date: "00/00/00",
     name: "Bob - API",
     body: "here is my api body",
-    comments: [{body: "API comment", date: "0/0/00"}]
+    comments: [{comment_body: "API comment", comment_date: "0/0/00"}],
+    id: 1
   },
   {
-    id: 2,
+    date: "00/00/00",
     name: "Julie - API",
-    username: "here is my api body",
-    comments: [{body: "API comment", date: "0/0/00"}]
+    body: "here is my api body",
+    comments: [{comment_body: "API comment", comment_date: "0/0/00"}],
+    id: 2
   }
 ];
 
@@ -37,6 +41,42 @@ var posts = [
 app.get("/api/posts", function(req, res) {
   res.json(posts);
 });
+
+// set up route for /users/id JSON
+app.get("/api/posts/:id", function(req, res) {
+
+  // set the value of the id
+  var targetId = parseInt(req.params.id);
+
+  // find item in `phrases` array matching the id
+  var foundPost = _.findWhere(posts, {id: targetId});
+
+  res.json(foundPost);
+
+});
+
+// create new phrase
+app.post('/api/posts', function (req, res) {
+  // grab params (word and definition) from form data
+  var newPost = req.body;
+
+  // set sequential id (last id in `phrases` array + 1)
+  if (posts.length > 0) {
+    newPost.id = posts[posts.length - 1].id +  1;
+  } else {
+    newPost.id = 0;
+  }
+
+  // add newPhrase to `phrases` array
+  posts.push(newPost);
+  
+  // send newPhrase as JSON response
+  res.json(newPost);
+});
+
+
+
+
 
 
 // listen on port 3000
--- public/scripts/script.js
+++ public/scripts/script.js
@@ -19,6 +19,15 @@ $(function() {
 
 	var $postsContainer = $("#posts-container");
 
+	// Edit Post Variables
+	var $editPostForm = $("#edit-post-form");
+	var $editPostModal = $("#edit-post-modal");
+
+	var $editPostName = $("#edit-post-name");
+	var $editPostBody = $("#edit-post-body");
+
+	var editPostId;
+
 	// Comment Variables
 	var $commentInput = $(".comment-input");
 	var $commentSubmit = $(".comment-submit");
@@ -33,19 +42,34 @@ $(function() {
 
 	// Contrsuctor
 	var Post = function(name, body) {
+		this.id;
 		this.name = name;
 		this.body = body;
-		this.date;
-		this.comments = [];
+		this.date = "no date yet";
+		this.comments = [{comment_body: "no comment yet", comment_date: "no date yet"}];
 	}
 
 	// Posts array - ask about why this has to be outside of the constructor and why not just use a reqular array
-	Post.all = [];
+	// Post.all = [];
+	// var posts = [];
+
 
 	// Save to array
-	Post.prototype.savePost = function() {
-		Post.all.unshift(this);
-	};
+	// Post.prototype.savePost = function() {
+		// Post.all.unshift(this);
+
+		// var postName = $postName.val();
+		// var postBody = $postBody.val();
+		// var postData = new Post(postName, postBody);
+		// var postData = {name: postName, body: postBody};
+
+		// $.post('/api/posts', postData, function(data) {
+		  // pass phrase object through template and append to view
+		  // var $phraseHtml = $(phrasesController.template(data));
+		  // $('#phrase-list').append($phraseHtml);
+		// });
+
+	// };
 
 	//Render on page
 	Post.prototype.render = function() {
@@ -53,14 +77,17 @@ $(function() {
 		$postsContainer.prepend($post);
 
 		//add index
-		var index = Post.all.indexOf(this);
-		$post.attr("data-index", index);
+		// var index = Post.all.indexOf(this);
+		// $post.attr("data-index", index);
 	};
 
 	var Comment = function(body) {
 		this.body = "has no body yet";
-		this.date;
+		this.date = "has no date yet";
 	}
+
+
+	// Pre-API Testing
 
 	// Test Posts
 	// var post1 = new Post("henry", "1: Do you like green eggs and ham? I do not like them");
@@ -79,49 +106,108 @@ $(function() {
 	// post3.savePost();
 
 	// Render Array on page
-	Post.all.reverse();
-	_.each(Post.all, function(post, index) {
-		post.render();
-	})
+	// Post.all.reverse();
+	// _.each(Post.all, function(post, index) {
+	// 	post.render();
+	// })
 
-	// Update post count
-	$postCount.html(Post.all.length + " posts")
+	// API, Render on page load
+	$.ajax({
+		url: "http://localhost:3000/api/posts",
+		type: "GET",
+		success: function(data) {
 
-	// on modal SHOW
+			numPosts = data.length;
+			// console.log(data);
+			// console.log(numPosts);
+
+			// var template = _.template($("#user-template").html());
+
+			// _.each(data, function(user) {
+			// 	$(".user-table tbody").append(template(user));
+			// });
+
+			// data.reverse();
+			_.each(data, function(post) {
+
+				var $post = $(postTemplate(post));
+				$postsContainer.prepend($post);
+
+				// posts.unshift($post);
+				// user.render();
+			})
+
+			console.log("--> number of posts")
+			console.log(numPosts);
+
+			// Update post count
+			$postCount.html(numPosts + " posts");
+
+		}
+	});
+
+	// on new post modal SHOW
 	$newPostModal.on('shown.bs.modal', function () {
 	  $postName.focus();
 	})
 
-	// on modal HIDE
+	// on new post modal HIDE
 	$newPostModal.on('hidden.bs.modal', function(){
 	    $(this).find('form')[0].reset();
 	});
 
-	// On CLICK not SUBMIT - ask why this isn't working as a submit listener
+	// On SUBMIT of Post form
 	$newPostForm.on("submit", function(event) {
 		event.preventDefault();
 
 		// Temporary variables
 		var postName = $postName.val();
 		var postBody = $postBody.val();
-		var postData = new Post(postName, postBody);
+		var postData = {name: postName, body: postBody};
+		// var postData = new Post(postName, postBody);
 
 		// add date
 		var date = new Date();
+		// postData.date = date.toLocaleString();
 		postData.date = date.toLocaleString();
 
+		// add comments
+		postData.comments = [{comment_body: "no comment yet", comment_date: "no date yet"}];
+
 		if (postName != "" && postBody != "") {
-			$newPostModal.modal('hide')
+			$newPostModal.modal("hide");
 
-			// save to Post.all array
-			postData.savePost();
+			// render on client side
+			// var $post = $(postTemplate(postData));
+			// $postsContainer.prepend($post);
 
-			//Render on page
-			postData.render();
+			//add to array
+			// posts.unshift($post);
 
-			// Update post count
-			$postCount.html(Post.all.length + " posts")
+			// // add temp ID
+			postData.id = numPosts + 1;
+
+			// render on client side
+			var $post = $(postTemplate(postData));
+			$postsContainer.prepend($post);
+
+			$.ajax({
+				type: "POST",
+				url: "http://localhost:3000/api/posts",
+				data: postData,
+				success: function(data) {
+
+					// add temp ID
+					// postData.id = data.id;
+					
+				},
+				error: function() {
+					alert("Error!");
+				}
+			});
+
 		} else {
+
 			$("#post-name-group").addClass("has-error");
 			$("#post-body-group").addClass("has-error");
 
@@ -129,6 +215,42 @@ $(function() {
 
 			console.log("you gotta type something")
 		}
+
+	});
+
+	// On click of edit post button
+	$postsContainer.on("click", ".edit-post", function(event) {
+		event.preventDefault();
+
+		console.log("--> you are going to edit this post!")
+
+		console.log("--> you clicked")
+		console.log(this);
+
+		$.ajax({
+			url: "http://localhost:3000/api/posts/" + $(this).closest(".panel").data("id"),
+			type: "GET",
+			success: function(data) {
+				$editPostName.val(data.name);
+				$editPostBody.val(data.body);
+
+				console.log(data.id)
+				editPostId = data.id
+			},
+				error: function() {
+					alert("Error!");
+			}
+		});
+
+	});
+
+	// PUT and render Post edits
+	$editPostForm.on("submit", function(event) {
+		event.preventDefault();
+
+		$editPostModal.modal("hide");
+
+		
 
 	});
 
@@ -143,13 +265,13 @@ $(function() {
 		var comment = $commentInput.val();
 
 		// generate new Comment instance
-		var newComment = new Comment();
-		newComment.body = comment;
+		var newComment = {comment_body: comment};
+		// newComment.body = comment;
 		console.log(newComment)
 
 		// add date
 		var date = new Date();
-		newComment.date = date.toLocaleString();
+		newComment.comment_date = date.toLocaleString();
 
 		// reset input
 		$commentInput.val("");
